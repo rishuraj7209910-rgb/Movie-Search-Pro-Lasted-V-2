1a440e6feaaa9251e4ddf814ab3c2a3018eafc11
Satyajeetkumarofficial <152394869+Satyajeetkumarofficial@users.noreply.github.com>
2025-02-09 14:25:01 +0530

    Update Redeem.py

--- plugins/Extra/Redeem.py
+++ plugins/Extra/Redeem.py
@@ -44,7 +44,7 @@ Aᴍᴏᴜɴᴛ:</b> {num_codes}
 
         keyboard = InlineKeyboardMarkup(
             [
-                [InlineKeyboardButton("♻️ Redeem Here ♻️", url="https://t.me/{}>{}")],
+                [InlineKeyboardButton("♻️ Redeem Here ♻️", url="https://t.me/ProBotsUpdate")],
                 [InlineKeyboardButton("❕ Any Query ❕", url="https://t.me/Satyajeetkumarofficial")]
             ]
         )
