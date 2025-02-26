9c79261dd9dfbb382e5af36336743c98cd5345ab
Satyajeetkumarofficial <152394869+Satyajeetkumarofficial@users.noreply.github.com>
2025-02-26 17:43:03 +0530

    Update info.py

--- info.py
+++ info.py
@@ -144,7 +144,7 @@ admin_cmds = [
     "/delreq", "/channel", "/del_file", "/delete",
     "/deletefiles", "/deleteall", 
     "All These Commands Can Be Used Only By Admins.", 
-    "⚡ powered by @ProBotsUpdate"
+    "⚡ powered by @ProBotUpdate"
 ]
 
 cmds = [
