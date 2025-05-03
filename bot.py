7341f5136aa5e02b5228312cff6c9a7965469e9d
Satyajeetkumarofficial <152394869+Satyajeetkumarofficial@users.noreply.github.com>
2025-05-03 12:59:59 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -44,7 +44,7 @@ files = glob.glob(ppath)
 JisshuBot.start()
 loop = asyncio.get_event_loop()
 
-pyrogram.utils.MIN_CHANNEL_ID = -1002256697098
+pyrogram.utils.MIN_CHANNEL_ID = -1002503371990
 
 async def Jisshu_start():
     print('\n')
